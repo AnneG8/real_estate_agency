6aad4f48f068cba365bd9ec7bed741d669eadf6b
Anna Veselova <anna.veselova.97@mail.ru>
2023-08-01 23:17:16 +0300

    upgrade filling Flat.new_building

--- property/migrations/0004_auto_20230727_2125.py
+++ property/migrations/0004_auto_20230727_2125.py
@@ -5,15 +5,10 @@ from django.db import migrations
 
 def fill_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        new_building = True if flat.construction_year >= 2015 else False
-        Flat.objects.update_or_create(
-            town=flat.town,
-            address=flat.address,
-            owner=flat.owner,
-            defaults={'new_building': new_building}
-        )
-
+    Flat.objects.filter(construction_year__gte=2015)
+                .update(new_building=True)
+    Flat.objects.exclude(construction_year__gte=2015)
+                .update(new_building=False)
 
 
 class Migration(migrations.Migration):
